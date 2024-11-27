0f74ec3e2cf9b1a9506f1be468e792ceb6befefd
kanth-sri <srikanthadepu8008@gmail.com>
2024-11-27 19:31:35 +0530

    jenkinsfiles

--- 30-rds/data.tf
+++ 30-rds/data.tf
@@ -4,7 +4,7 @@ data "aws_ssm_parameter" "mysql_sg_id" {
 }
 
 data "aws_ssm_parameter" "database_subnet_group_name" {
-  name = "/${var.project_name}/${var.environment}/database_subnet_group_name"
+  name = "/${var.project_name}/${var.environment}/db_subnet_group_name"
 }
 
 

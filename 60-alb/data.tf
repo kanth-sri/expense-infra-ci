96566e81b993d8f15024f862a5cdb5d9c1ad29d7
kanth-sri <srikanthadepu8008@gmail.com>
2024-11-27 19:51:12 +0530

    jenkinsfiles

--- 60-alb/data.tf
+++ 60-alb/data.tf
@@ -8,7 +8,7 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "ingress_alb_sg_id" {
-  name = "/${var.project_name}/${var.environment}/ingress_alb_sg_id"
+  name = "/${var.project_name}/${var.environment}/ingress_sg_id"
 }
 
 data "aws_ssm_parameter" "https_certificate_arn" {
